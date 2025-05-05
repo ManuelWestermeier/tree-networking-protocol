95771cdfb52c161ac6fe618f010b87f469658687
ManuelWestermeier <westermeier111@gmail.com>
2025-05-05 21:53:45 +0200

    loop build

--- build-and-upload.bat
+++ build-and-upload.bat
@@ -1,2 +1,6 @@
+:a
+cls
 "C:\Users\Manuel Westermeier\.platformio\penv\Scripts\platformio.exe" run --target upload --upload-port COM4
 "C:\Users\Manuel Westermeier\.platformio\penv\Scripts\platformio.exe" run --target upload --upload-port COM4
+pause
+goto a
